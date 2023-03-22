a4073af2190b7330aaa821d9691ab8639de28839
aaradhya_khanna <aaradhykhanna47@gmail.com>
2023-03-22 23:07:22 +0530

    First Entry level Android Project.
    A simple app that receives input from the user and then calculates and displays the tip according to the service procided.

---
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.tiptime
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        when (cost) {
+            null -> {
+                binding.tipResult.text = ""
+                return
+            }
+            else -> {
+                val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+                    R.id.option_twenty_percent -> 0.20
+                    R.id.option_eighteen_percent -> 0.18
+                    else -> 0.15
+                }
+
+                var tip = tipPercentage * cost
+                if (binding.roundUpSwitch.isChecked) {
+                    tip = kotlin.math.floor(tip)
+                }
+
+                val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+                binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+            }
+        }
+
+    }
+}
